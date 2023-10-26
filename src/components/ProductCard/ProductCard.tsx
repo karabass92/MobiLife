97311e6697f2aaee6b583d9d0210a47865b2826e
karabass92 <shatveryank@gmail.com>
2023-10-26 17:00:48 +0600

    Реализован компонент ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,8 +1,28 @@
-const ProductCard = () => {
+import LinkButton from '../Button/LinkButton/LinkButton';
+import style from './ProductCard.module.scss';
+
+
+type Props = {
+    id: number,
+    img?: string,
+    name: string,
+    price: string
+};
+
+
+const ProductCard = ({
+    img, 
+    name, 
+    price, 
+    id}: Props) => {
+
     return (
-        <div>
-            ProductCard
-        </div>
+        <section className={style.main}>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>&#8376; {price}</p>
+            <LinkButton text='Подробнее' link={`../product/${id}`} width={127} height={41} />
+        </section>
     );
 };
 
