923c1f60e039c996d62adb0374f885df60d6414e
karabass92 <shatveryank@gmail.com>
2023-11-03 12:10:22 +0600

    Верстка компонента ProductCountToCart

--- src/components/ProductCountToCart/ProductCountToCart.tsx
+++ src/components/ProductCountToCart/ProductCountToCart.tsx
@@ -11,15 +11,24 @@ const ProductCountToCart = ({
     productCount,
     setProductCount
 }: Props) => {
+
     return (
         <div className={style.main}>
             <span>Количество:</span>
-            <input 
-                type="number" 
-                className={style.productCount}
-                defaultValue={productCount} 
-                //value={productCount} 
-                onChange={() => setProductCount()} /> 
+            <div className={style.productCount}>
+                <button 
+                    onClick={() => setProductCount(productCount - 1)}
+                    disabled={productCount <= 1} >
+                    -
+                </button>
+                <span className={style.count}>{productCount}</span>
+                <button 
+                    onClick={() => setProductCount(productCount + 1)}
+                    disabled={productCount >= 9} >
+                    +
+                </button> 
+            </div>
+
         </div>
     );
 };
