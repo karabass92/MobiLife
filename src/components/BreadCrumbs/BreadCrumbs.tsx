c851b3812be11e37fb3393843fe728430664c23d
karabass92 <shatveryank@gmail.com>
2023-11-03 10:02:44 +0600

    Фикс компонента BreadCrumbs, адаптация под страницу Product

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,20 +1,31 @@
+import { Fragment } from 'react';
 import { Link } from 'react-router-dom'
 import style from './BreadCrumbs.module.scss';
 
 
 type Props = {
     header: string,
+    product?: string
 };
 
 
 const BreadCrumbs = ({
     header,
+    product
 }: Props) => {
     return (
         <div className={style.breadCrumbs}>
             <h1>{header}</h1>
             <div className={style.path}>
-                <Link to={'/'}>Главная</Link> — <span>{header}</span>
+                <Link to={'/'}>Главная</Link>
+                {
+                    product
+                    ?   <Fragment>
+                            <Link to={'/shop'}>{` — ${header}`}</Link>
+                            <span>{` — ${product}`}</span>
+                        </Fragment>
+                    : <span>{` — ${header}`}</span>
+                }    
             </div>
         </div>
     );
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,11 +1,20 @@
-import { Fragment } from 'react';
+import BreadCrumbs from '../../components/BreadCrumbs/BreadCrumbs';
+import style from './Product.module.scss';
 
 
 const Product = () => {
+
+    const product = {
+            id: 1,
+            name: 'товар 1',
+            price: '1000',
+        };
+
     return (
-        <Fragment>
+        <main className={style.main}>
+            <BreadCrumbs header='Каталог' product={product.name} />
             Product
-        </Fragment>
+        </main>
     )
 };
 
