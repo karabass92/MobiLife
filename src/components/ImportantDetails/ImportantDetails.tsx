e89be1a79337df094e970079f9fdb6fa082cedaa
karabass92 <shatveryank@gmail.com>
2023-12-28 10:22:18 +0600

    Введен компонент ImportantDetails, адаптивная верстка, внедрение на страницу Main

---
+++ src/components/ImportantDetails/ImportantDetails.tsx
@@ -0,0 +1,42 @@
+import important1 from '../../assets/img/ImportantDetail/important_1.png'
+import important2 from '../../assets/img/ImportantDetail/important_2.png'
+import important3 from '../../assets/img/ImportantDetail/important_3.png'
+import LinkButton from '../Button/LinkButton/LinkButton'
+import style from './ImportantDetails.module.scss'
+
+
+const ImportantDetails = () => {
+    return (
+        <section className={style.main}>
+            <h2 className={style.sectionHeader}>Что для нас важно</h2>
+            <section className={style.importantBlock}>
+                <article className={style.importantItem}>
+                    <img src={important1} alt="img" />
+                    <h3>Ассортимент</h3>
+                    <p>
+                        Мобильные аксуссуары, зарядные устройства, чехлы для Ваших устройсв, и многое другое
+                    </p>
+                </article>
+                <article className={style.importantItem}>
+                    <img src={important2} alt='img' />
+                    <h3>Удобство</h3>
+                    <p>
+                        Оплата Kaspi Red, возможность покупки в рассрочку (0-0-12), бесплатная доставка
+                    </p>
+                </article>
+                <article className={style.importantItem}>
+                    <img src={important3} alt="img" />
+                    <h3>Ответственность</h3>
+                    <p>
+                        Мы заботимся о наших клиентах. Грамотная консультация, компетентные и 
+                        вежливые сотрудники - все это MobiLife
+                    </p>
+                </article>
+            </section>
+            <LinkButton height={60} width={200} text='О нас' key={'about'} link='/about' />
+        </section>
+    )
+}
+
+
+export default ImportantDetails
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,20 @@
-import style from './Main.module.scss';
+import ImportantDetails from '../../components/ImportantDetails/ImportantDetails'
+import style from './Main.module.scss'
 
 
 const Main = () => {
     return (
         <main className={style.main}>
-            Main
+            <div>
+                BANNER
+            </div>
+            <div>
+                NEW PRODUCTS
+            </div>
+            <ImportantDetails />
         </main>
     )
-};
+}
 
 
-export default Main;
+export default Main
